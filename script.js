6b58ed86c16c58548fa90e65b1d02365dc8e695b
Tejas Naik <RNTejas2005@gmail.com>
2022-12-04 10:24:28 +0530

    Challenge #1 : BMI

---
+++ script.js
@@ -0,0 +1,12 @@
+// Challenge #1
+const marksMass = 78;
+const marksHeight = 1.69;
+const johnsMass = 92;
+const johnsHeight = 1.95;
+
+const marksBMI = marksMass / (marksHeight * 2);
+const johnsBMI = johnsMass / (johnsHeight * 2);
+
+const markHigherBMI = marksBMI > johnsBMI;
+console.log(markHigherBMI);
+
