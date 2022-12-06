882f3688ced46be4547fde1c1d4b1c0ded4044e6
Tejas Naik <RNTejas2005@gmail.com>
2022-12-07 01:20:25 +0530

    Maps destructuring/iterating

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ console.log(
     calcAvarage(totals),
 );
 */
-
+/*
 // football betting app.
 const game = {
     team1: 'Bayern Munich',
@@ -256,3 +256,38 @@ for (let x of game.scored) {
 }
 
 console.log(scorers);
+*/
+
+// 3) 
+const gameEvents = new Map([
+    [17, '⚽ GOAL'],
+    [36, '� Substitution'],
+    [47, '⚽ GOAL'],
+    [61, '� Substitution'],
+    [64, '� Yellow card'],
+    [69, '� Red card'],
+    [70, '� Substitution'],
+    [72, '� Substitution'],
+    [76, '⚽ GOAL'],
+    [80, '⚽ GOAL'],
+    [92, '� Yellow card'],
+]);
+
+// 1)
+const events = []
+for (const [time, event] of gameEvents) {
+    events.push(event);
+}
+console.log([...new Set(events)]);
+
+// 2) 
+gameEvents.delete(64);
+
+// 3)
+console.log(`An event happened on an avarage of ${Array.from(gameEvents.keys()).pop() / gameEvents.size}`);
+
+// 4) 
+for (const [time, event] of gameEvents) {
+    const half = time <= 45 ? "FIRST" : "SECOND";
+    console.log(`[${half}HALF] ${time} : ${event}`);
+}
